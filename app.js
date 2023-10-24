96ecb998e6010d6ffdb6521bb1b27e574c367464
Ester-0 <azalea_@hotmail.com>
2023-10-24 12:28:49 +0200

    ok

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 const num1 = Math.floor(Math.random() * 10);
 const num2 = Math.floor(Math.random() * 10);
 
-document.querySelector("#primary-number").value = num1;
-document.querySelector("#secondary-number").value = num2;
+document.querySelector("#primary-number").textContent = num1;
+document.querySelector("#secondary-number").textContent = num2;
 
 // 2. Haz la suma de dichos números y guardala en otra variable
 const sum = num1 + num2;
@@ -15,18 +15,18 @@ console.log("esto suma " + sum);
 const button = document.querySelector("#btn");
 
 button.addEventListener("click", function () {
-    const sumGuess = +document.querySelector("#guess").value;
+    const sumGuess = Number(document.querySelector("#guess").value);
     let message = document.querySelector("#message").innerHTML;
 
     if (sum == sumGuess) {
-        //message = "La suma es correcta!";
+        //document.querySelector("#message").textContent = "La suma es correcta!"; o usado el .innerHTML. Este sirve para lo mismo pero se puede añadir una etiqueta span meter estilos como color, size,...
+        document.querySelector("#message").innerHTML = "La suma es correcta!";
 
-        document.querySelector("#message").innerHTML = message.replace("La suma es correcta! CAmbia este mensaje en función de si la suma es correcta o incorrecta cuando el usuario haga click en el botón", "La suma es correcta!")
-        
-        console.log ("mensaje" + message)
+        console.log("mensaje" + message);
 
-    } else { //message = "La suma es incorrecta!";
-        document.querySelector("#message").innerHTML = message.replace("La suma es correcta! CAmbia este mensaje en función de si la suma es correcta o incorrecta cuando el usuario haga click en el botón","La suma es incorrecta!")
+    } else {
+        //document.querySelector("#message").textContent = "La suma es incorrecta!";
+        document.querySelector("#message").innerHTML = "La suma es incorrecta!";
     }
 
     console.log("esto es un if mas un sumgess " + sumGuess);
